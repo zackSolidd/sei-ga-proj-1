6574740258df1e30d79151245e0bd7c528726d46
zackSolidd <zacktjc@gmail.com>
2020-06-28 04:12:22 +0800

    game timer countdown done. please fix double click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 console.log("hello world");
 
 let score = 0;
+let gameTime = 8;
+let gameState = 0; //0 press start to play, 1 press restart first then press 1;
 
 let stopBtn = document.createElement('button');
 stopBtn.setAttribute('class','buttonHolder')
@@ -13,57 +15,76 @@ restartBtn.setAttribute('class','buttonHolder')
 restartBtn.textContent = ("Restart");
 let container = document.querySelector('.container');
 let scoreBoard = document.createElement('p');
+let countDownTimer = document.createElement('p');
 
-scoreBoard.innerHTML = (`Score = ${score}`)
+scoreBoard.innerHTML = (`Score = ${score}`);
+countDownTimer.innerHTML = (`Timer = ${gameTime}`);
 
 container.appendChild(startBtn);
 container.appendChild(stopBtn);
 container.appendChild(restartBtn);
 container.appendChild(scoreBoard);
+container.appendChild(countDownTimer);
 
 let target = document.querySelectorAll(".target");
 //functions 
 function changeColor(){
-    for (let i = 0 ; i < target.length ; i++) {
-        target[i].style.backgroundColor = "white";
-    }
-    let randomNumber = Math.floor(Math.random()*16+1);
-    console.log(randomNumber);
-    let box = document.querySelector('.box'+randomNumber);
-    box.style.backgroundColor = 'red';
-    box.addEventListener('click', function(e) {
-        console.log(e.srcElement.className);
-        if ( e.srcElement.className == 'box'+randomNumber+' target') {
-            score++;
-            console.log('score =' + score);
-            scoreBoard.innerHTML = (`Score = ${score}`)
+    if ( gameState === 0 ) {
+        for (let i = 0 ; i < target.length ; i++) {
+            target[i].style.backgroundColor = "white";
         }
-        else {console.log('false')};
-    })
+        let randomNumber = Math.floor(Math.random()*16+1);
+        console.log(randomNumber);
+        let box = document.querySelector('.box'+randomNumber);
+        box.style.backgroundColor = 'red';
+        box.addEventListener('click', function(e) {
+            console.log(e.srcElement.className);
+            if ( e.srcElement.className == 'box'+randomNumber+' target') {
+                score++;
+                console.log('score =' + score);
+                scoreBoard.innerHTML = (`Score = ${score}`);
+                changeColor();
+            }
+        });
+    }
+    else {
+        alert("Please press Restart");
+    }
+};
 
-}
+var timerInterval;
 
-var colorTiming;
-
-function startTime() {
-    colorTiming= setInterval(changeColor,1000);
+function timerCountDownFunction() {
+    gameTime--;
+    countDownTimer.innerHTML = (`Timer = ${gameTime}`); 
+    if (gameTime === 0) {
+        alert(`Times Up !!! \n Your Score is ${score}`);
+        clearInterval(timerInterval);
+        gameState = 1;
+    }
 }
 
 // event listener
 startBtn.addEventListener('click', function() {
-    startTime()
+    //startTime();
+    setTimeout(changeColor,3000);
+    timerInterval = setInterval(timerCountDownFunction,1000);
 })
 
 stopBtn.addEventListener('click', function() {
-    clearInterval(colorTiming);
+    clearInterval(timerInterval);
+    gameState = 1;
 })
 
 restartBtn.addEventListener('click', function() {
     for (let i = 0 ; i < target.length ; i++) {
         target[i].style.backgroundColor = "white";
     }
-    clearInterval(colorTiming);
-    scoreBoard.innerHTML = (`Score = 0`)
+    score = 0;
+    gameTime = 8;
+    scoreBoard.innerHTML = (`Score = 0`);
+    countDownTimer.innerHTML = (`Timer = 5`);
+    gameState = 0;
 })
 
 
